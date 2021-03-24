1e87aa9304d07eabc9ce92978a28408d20b916d8
kazumasamatsumoto <k.matsumoto@benjamin.co.jp>
2021-03-24 11:08:14 +0900

    RTL

---
+++ src/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+
+    // fails
+    // expect(screen.getByText('Search')).toBeInTheDocument();
+
+    // succeeds
+    expect(screen.getByText('Search:')).toBeInTheDocument();
+
+    // succeeds
+    expect(screen.getByText(/Search/)).toBeInTheDocument();
+  });
+});
